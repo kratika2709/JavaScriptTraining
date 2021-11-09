ba3165d6bdd50d7c45a173e99858d824254354ef
kratika2709 <chouhan.kratika2709@gmail.com>
2021-11-09 17:36:04 +0530

    Objects code added

--- force-app/main/default/JavaScriptCode/Arrays.js
+++ force-app/main/default/JavaScriptCode/Arrays.js
@@ -1,4 +1,13 @@
-const someNumbers =[11,22,33,44];
+const someNumbers =[11,22,33,44,55,66,77,88];
 console.log(someNumbers);
 someNumbers.push(99);
 console.log(someNumbers);
+
+someNumbers.pop(99);
+console.log(someNumbers); 
+
+console.log(someNumbers.slice(0,3));
+
+someNumbers.sort();
+console.log(someNumbers);
+
---
+++ force-app/main/default/JavaScriptCode/objects.js
@@ -0,0 +1,8 @@
+
+// objects are key-value pairs in java script.
+const student = {
+    name: "Kratika",
+    age: 22,
+    isPresent : true
+}
+console.log(student);
