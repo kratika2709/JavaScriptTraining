87a3cda0356930fcc191862a35d0c1f0c73ccea7
kratika2709 <chouhan.kratika2709@gmail.com>
2021-11-09 18:26:01 +0530

    objects code edited

--- force-app/main/default/JavaScriptCode/objects.js
+++ force-app/main/default/JavaScriptCode/objects.js
@@ -3,6 +3,23 @@
 const student = {
     name: "Kratika",
     age: 22,
-    isPresent : true
+    isPresent : true,
+    fees : undefined,
+    datesWhenAbsent:[1,2,3]
 }
+console.log(Object.entries(student));
+
+console.log(Object.keys(student));
+
+console.log(Object.values(student));
+
+console.log(student.age);
+
+console.log(student["age"]);
+
 console.log(student);
+
+delete student.fees;
+console.log(student);
+
+console.log("jhon was absent on",student["datesWhenAbsent"]);
