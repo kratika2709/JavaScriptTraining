ada14b894949e34b7b722d513d8511a63cb770d5
kratika2709 <chouhan.kratika2709@gmail.com>
2021-11-09 16:54:13 +0530

    Arrays code added

---
+++ force-app/main/default/JavaScriptCode/Arrays.js
@@ -0,0 +1,4 @@
+const someNumbers =[11,22,33,44];
+console.log(someNumbers);
+someNumbers.push(99);
+console.log(someNumbers);
--- force-app/main/default/JavaScriptCode/firstCode.js
+++ force-app/main/default/JavaScriptCode/firstCode.js
@@ -1,1 +1,18 @@
 console.log("Hello Kratika");
+console.error("Error Occured");
+console.warn("Its a warning");
+
+// Declaring a variables (var)
+for (var i=0;i<=10;i++){}
+console.log(i);
+
+//(let)
+for (let i=0;i<=10;i++){
+    console.log(i);
+}
+
+const newnumber = 99;
+newnumber = 100; // we cannot redeclare a value of constant.
+console.log(newnumber);
+
+
