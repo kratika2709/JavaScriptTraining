afb2658575d52d2b3612088f8d7b0fd64a77caaf
kratika2709 <chouhan.kratika2709@gmail.com>
2021-11-09 19:28:42 +0530

    code edited

--- force-app/main/default/JavaScriptCode/objects.js
+++ force-app/main/default/JavaScriptCode/objects.js
@@ -5,8 +5,31 @@ const student = {
     age: 22,
     isPresent : true,
     fees : undefined,
-    datesWhenAbsent:[1,2,3]
+
+    //An objects can store arrays.
+    datesWhenAbsent:[1,2,3],
+    //An object can store other objects as well.
+    address:{
+        street:"Bombay street",
+        pincode: '65879',
+        city :"Bombay",
+    },
 }
+
+
+const students = [student,{
+    name: "abc",
+    age:"33",
+    fees: undefined,
+    isPresent : true,
+    datesWhenAbsent:[4,2,8],
+    address:{
+        street:"chennai street",
+        pincode: '98980',
+        city :"Chennai",
+    },
+}];
+
 console.log(Object.entries(student));
 
 console.log(Object.keys(student));
@@ -23,3 +46,5 @@ delete student.fees;
 console.log(student);
 
 console.log("jhon was absent on",student["datesWhenAbsent"]);
+
+console.log(students);
